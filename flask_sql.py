f883c45b343f7a7087abba6dd40284dfa9f386ef
zarazoya <zara.zoya09@gmail.com>
2019-12-13 16:50:16 +0500

    SQL Flask file

--- flask_sql.py
+++ flask_sql.py
@@ -1,12 +1,10 @@
 from flask import Flask,jsonify
 import sqlite3 as sq
 
-conn = sq.connect("ApiDatabase.db")
-c = conn.cursor()
 
 #creating Table
 
-c.execute(""" CREATE TODO LIST TABLE (
+c.execute(""" CREATE TABLE employee (
     id integer,
     title text,
     description text,
@@ -14,14 +12,24 @@ c.execute(""" CREATE TODO LIST TABLE (
     ) """)
 conn.commit()
 
+
 app = Flask(__name__)
 
 @app.route("/getapi")
 def getapi():
     conn = sq.connect("ApiDatabase.db")
-    #creating database file with the name ZoyaDataBase
+    #creating database file with the name UsmanDataBase
     c = conn.cursor()
     c.execute("SELECT * FROM employee")
+    data = c.fetchall()
+    return jsonify({"data":data})
+
+@app.route("/getsingleapi/<idx>",methods=['GET'])
+def getsingleapi(idx):
+    conn = sq.connect("ApiDatabase.db")
+    #creating database file with the name UsmanDataBase
+    c = conn.cursor()
+    c.execute("SELECT * FROM employee WHERE id = ?",(idx,))
     data = c.fetchall()
     return jsonify({"data":data})
 
