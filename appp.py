c512524d4831be97dbe4127e410ee62afee3f5bd
zarazoya <zara.zoya09@gmail.com>
2019-12-13 15:02:17 +0500

    Boot Camp task using python

--- appp.py
+++ appp.py
@@ -25,3 +25,19 @@ def Getalldata():
         i.pop('_id')
         ls.append(i)
     return jsonify(ls)
+
+    #GET API FOR SINGLE RECORD
+@app.route('/GETSINGLEDATA/<idx>', methods= ['GET']) 
+def GetSingleData(idx):
+    x = collection.find_one({"id": idx})
+    data = {}
+    for i, v in x.items():
+        if i != '_id':
+            data[i] = v
+    print(data)
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug= True)
+
