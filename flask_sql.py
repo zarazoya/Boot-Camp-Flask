399e10fee4427e6a76df2d543da6454dac8b1be8
zarazoya <zara.zoya09@gmail.com>
2019-12-13 16:33:41 +0500

    SQL File

--- flask_sql.py
+++ flask_sql.py
@@ -14,6 +14,17 @@ c.execute(""" CREATE TODO LIST TABLE (
     ) """)
 conn.commit()
 
+app = Flask(__name__)
+
+@app.route("/getapi")
+def getapi():
+    conn = sq.connect("ApiDatabase.db")
+    #creating database file with the name ZoyaDataBase
+    c = conn.cursor()
+    c.execute("SELECT * FROM employee")
+    data = c.fetchall()
+    return jsonify({"data":data})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
